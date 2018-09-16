0a6305f690b7b97b0d576d82b5eceb73b3275b26
DikarevAndrey <dias7707@gmail.com>
2018-09-16 16:17:21 +0300

    Created Xcode project

---
+++ DZ1/DZ1/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  DZ1
+//
+//  Created by Andrey Dikarev on 16/09/2018.
+//  Copyright © 2018 Andrey Dikarev. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
